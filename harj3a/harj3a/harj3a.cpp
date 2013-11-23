ff129f18676425cd2e0e7e793f0a589b5d1ede13
Leisi <Leisi93@gmail.com>
2013-11-23 22:45:30 +0200

    Valmis

---
+++ harj3a/harj3a/harj3a.cpp
@@ -0,0 +1,25 @@
+/*****************
+ Tehtävä: Harjoitus 3 a
+ Tekijä: Joni Laine
+ PVM: 10.9.2013
+
+ Kuvaus:
+#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "Hei maailma!";
+}
+
+Muokkaa ylla olevaa ohjelmaa niin, etta tulostat oman nimesi
+ja osoitteesi naytolle allekkain.
+HUOM! Nimeä tiedosto harj3a.cpp nimelle!
+
+***************************************************/
+#include <iostream>
+using namespace std;
+void main (void)
+{
+	cout << "Joni"<<endl;
+	cout << "Oma osoite"<<endl;
+}
---
+++ harj3b/harj3b/harj3b.cpp
@@ -0,0 +1,36 @@
+/*****************
+ Tehtävä: Harjoitus 3 b
+ Tekijä: Joni Laine
+ PVM: 10.9.2013
+
+ Kuvaus:
+ Muokkaa edellisestä ohjelmasta versio nimelle harj3b.cpp, jossa
+ nimi ja osoite tulostetaan näytälle seuraavasti:
+
+**********************************
+* *
+* Etunimi *
+* Sukunimi *
+* *
+*-------------------------------------- *
+* *
+* Asemakatu 7 *
+* 40520 JKL *
+* *
+***************************************************/
+#include <iostream>
+using namespace std;
+void main (void)
+{
+	cout << "********************************" <<endl;
+	cout << "* *"<<endl;
+	cout << "* Oma etunimi *" <<endl;
+	cout << "* Oma sukunimi *" <<endl;
+	cout << "* * " <<endl;
+	cout << "*------------------------------*" <<endl;
+	cout << "* *" <<endl;
+	cout << "* Osoite *" <<endl;
+	cout << "* Postiosoite *" <<endl;
+	cout << "* *" <<endl;
+	cout << "********************************"<<endl;
+}
